abdf01a766446f380d56579e408a0863272691d2
lohsts <>
2026-07-15 19:11:10 +0100

    Test the advanced issuing path with an injected witness fixture

--- tests/lazer_protocol.rs
+++ tests/lazer_protocol.rs
@@ -8,14 +8,18 @@ use blind_sig::commitment_proof::{
     CommitmentProofProvider, LazerD64CommitmentProof, LazerD64CommitmentProofProvider,
 };
 use blind_sig::issue::{
-    SignerResponse, UserCommitMessage, signer_respond_with_provider, user_check,
+    IssueError, PreimageSampler, SignerResponse, TagSampler, UserCommitMessage, UserState,
+    signer_respond_with_components, signer_respond_with_provider, user_check,
     user_commit_with_provider, verify_user_commit_with_provider,
 };
 use blind_sig::keys::{KeyGenParams, PublicKey, SecretKey, key_gen};
 use blind_sig::lazer_ffi::Error;
 use blind_sig::proof_com::ComProofParams;
-use blind_sig::signature::{finalize, verify};
-use blind_sig::tag_function::{HashToRing, TagFunction};
+use blind_sig::signature::{
+    LazerD64FinalSignatureProofProvider, finalize, finalize_with_provider, verify,
+    verify_with_provider,
+};
+use blind_sig::tag_function::{BinaryEncoding, HashToRing, TagFunction};
 use qfall_math::integer::{MatPolyOverZ, PolyOverZ, Z};
 use qfall_math::integer_mod_q::{MatPolynomialRingZq, ModulusPolynomialRingZq};
 use qfall_math::rational::Q;
@@ -30,6 +34,31 @@ const Q_MOD: u64 = 281_474_976_711_349;
 struct FixedTagFunction {
     modulus: ModulusPolynomialRingZq,
     output: RefCell<MatPolynomialRingZq>,
+}
+
+struct FixedTagSampler {
+    x: Z,
+}
+
+impl TagSampler for FixedTagSampler {
+    fn sample_tag(&self, _domain_size: &Z) -> Result<Z, IssueError> {
+        Ok(self.x.clone())
+    }
+}
+
+struct FixturePreimageSampler {
+    s: MatPolyOverZ,
+}
+
+impl PreimageSampler for FixturePreimageSampler {
+    fn sample<F: TagFunction>(
+        &self,
+        _pk: &PublicKey<F>,
+        _sk: &SecretKey,
+        _target: &MatPolynomialRingZq,
+    ) -> Result<MatPolyOverZ, IssueError> {
+        Ok(self.s.clone())
+    }
 }
 
 impl FixedTagFunction {
@@ -115,6 +144,55 @@ fn d64_keys() -> (PublicKey<FixedTagFunction>, SecretKey) {
     )
 }
 
+fn d64_binary_fixture() -> (
+    PublicKey<BinaryEncoding>,
+    SecretKey,
+    MatPolyOverZ,
+    MatPolyOverZ,
+    MatPolyOverZ,
+) {
+    let psf = psf(D);
+    let modulus = psf.gp.modulus.clone();
+    let mut a = MatPolyOverZ::new(1, 51);
+    let mut x = PolyOverZ::default();
+    x.set_coeff(1, 1).unwrap();
+    a.set_entry(0, 0, x).unwrap();
+
+    let mut b1 = MatPolyOverZ::new(1, 2);
+    b1.set_entry(0, 0, PolyOverZ::from(1)).unwrap();
+    let mut b2 = MatPolyOverZ::new(1, 2);
+    b2.set_entry(0, 0, PolyOverZ::from(1)).unwrap();
+    let pk = PublicKey {
+        a: MatPolynomialRingZq::from((&a, &modulus)),
+        ck: CommitmentKey {
+            b1: MatPolynomialRingZq::from((&b1, &modulus)),
+            b2: MatPolynomialRingZq::from((&b2, &modulus)),
+        },
+        f: BinaryEncoding::new(1, D, modulus),
+        psf,
+        s_r: Q::from(3),
+        beta_msg_sqrd: Z::from(16),
+        beta_r_sqrd: Z::from(2000),
+        com_params: ComProofParams {
+            witness_inf: 20,
+            mask_inf: 8000,
+        },
+    };
+    let sk = SecretKey {
+        trapdoor: (MatPolyOverZ::new(1, 1), MatPolyOverZ::new(1, 1)),
+    };
+
+    let mut message = MatPolyOverZ::new(2, 1);
+    message.set_entry(0, 0, PolyOverZ::from(-3)).unwrap();
+    let mut randomness = MatPolyOverZ::new(2, 1);
+    randomness.set_entry(0, 0, PolyOverZ::from(1)).unwrap();
+    let mut preimage = MatPolyOverZ::new(51, 1);
+    let mut last = PolyOverZ::default();
+    last.set_coeff(D - 1, 2).unwrap();
+    preimage.set_entry(0, 0, last).unwrap();
+    (pk, sk, message, randomness, preimage)
+}
+
 fn sparse_message() -> MatPolyOverZ {
     let mut message = MatPolyOverZ::new(2, 1);
     message.set_entry(0, 0, PolyOverZ::from(1)).unwrap();
@@ -192,4 +270,44 @@ fn lazer_provider_rejects_the_standard_d8_profile() {
         &LazerD64CommitmentProofProvider::new([7; 32]),
     );
     assert!(matches!(result, Err(Error::ProfileMismatch(_))));
+}
+
+#[test]
+fn advanced_fixture_runs_both_lazer_proofs() {
+    let (pk, sk, message, randomness, preimage) = d64_binary_fixture();
+    let commitment = pk.ck.commit(&message, &randomness);
+    let commitment_provider = LazerD64CommitmentProofProvider::new([7; 32]);
+    let commitment_proof = commitment_provider
+        .prove(&pk, &message, &randomness, &commitment)
+        .expect("commitment proof");
+    let first_message = UserCommitMessage {
+        c: commitment.clone(),
+        proof: commitment_proof,
+    };
+    let state = UserState {
+        m: message.clone(),
+        r: randomness,
+        c: commitment,
+    };
+
+    let response = signer_respond_with_components(
+        &pk,
+        &sk,
+        &first_message,
+        &commitment_provider,
+        &FixedTagSampler { x: Z::ONE },
+        &FixturePreimageSampler { s: preimage },
+    )
+    .expect("fixture response");
+    assert!(user_check(&pk, &state, &response));
+
+    let signature_provider = LazerD64FinalSignatureProofProvider::new([8; 32]);
+    let signature = finalize_with_provider(&pk, state, response, &signature_provider)
+        .expect("final-signature proof");
+    assert!(verify_with_provider(
+        &pk,
+        &message,
+        &signature,
+        &signature_provider
+    ));
 }
